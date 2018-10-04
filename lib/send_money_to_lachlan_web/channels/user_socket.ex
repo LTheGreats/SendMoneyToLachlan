b425a54eccb28cfb3b106654c39caed570f4bfcd
LTheGreats <lachlan@wilgerfamily.com>
2018-10-03 18:01:22 -0700

    made changes needed to deploy

--- lib/send_money_to_lachlan_web/channels/user_socket.ex
+++ lib/send_money_to_lachlan_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule SendMoneyToLachlanWeb.UserSocket do
   # channel "room:*", SendMoneyToLachlanWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can

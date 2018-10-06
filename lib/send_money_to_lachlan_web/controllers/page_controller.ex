cc6f85737f6e1841dc90785c8c0d34887172cd86
LTheGreats <lachlan@wilgerfamily.com>
2018-10-05 21:01:06 -0700

    passes quote to the template

--- lib/send_money_to_lachlan_web/controllers/page_controller.ex
+++ lib/send_money_to_lachlan_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule SendMoneyToLachlanWeb.PageController do
   use SendMoneyToLachlanWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", quote: get_quote()
+  end
+
+  defp get_quote() do
+    File.read!("priv/Quotes.txt") |> String.split("\n") |> Enum.random()
   end
 end
